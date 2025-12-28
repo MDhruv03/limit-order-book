cda81609a09d76753c9441e56c5ae04136ac66ac
Praveen <praveen.sundriyal1111@gmail.com>
2025-12-28 17:00:39 +0530

    Update Order.cpp

--- Limit_Order_Book/Order.cpp
+++ Limit_Order_Book/Order.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 
 Order::Order(int _idNumber, bool _buyOrSell, int _shares, int _limit)
-    : idNumber(_idNumber), buyOrSell(_buyOrSell), shares(_shares), limit(_limit), 
-    nextOrder(nullptr), prevOrder(nullptr), parentLimit(nullptr) {}
+    : idNumber(_idNumber), buyOrSell(_buyOrSell), shares(_shares), limit(_limit),
+    nextOrder(nullptr), prevOrder(nullptr), parentLimit(nullptr) {
+}
 
 int Order::getShares() const
 {
@@ -33,57 +34,58 @@ Limit* Order::getParentLimit() const
 
 void Order::partiallyFillOrder(int orderedShares)
 {
+    // shares -= orderedShares;
+    // parentLimit->partiallyFillTotalVolume(orderedShares);
     shares -= orderedShares;
-    parentLimit->partiallyFillTotalVolume(orderedShares);
 }
 
 // Remove order from its parent limit
-void Order::cancel()
-{
-    if (prevOrder == nullptr)
-    {
-        parentLimit->headOrder = nextOrder;
-    } else
-    {
-        prevOrder->nextOrder = nextOrder;
-    }
-    if (nextOrder == nullptr)
-    {
-        parentLimit->tailOrder = prevOrder;
-    } else
-    {
-        nextOrder->prevOrder = prevOrder;
-    }
+// void Order::cancel()
+// {
+//     if (prevOrder == nullptr)
+//     {
+//         parentLimit->headOrder = nextOrder;
+//     } else
+//     {
+//         prevOrder->nextOrder = nextOrder;
+//     }
+//     if (nextOrder == nullptr)
+//     {
+//         parentLimit->tailOrder = prevOrder;
+//     } else
+//     {
+//         nextOrder->prevOrder = prevOrder;
+//     }
 
-    parentLimit->totalVolume -= shares;
-    parentLimit->size -= 1;
-}
+//     parentLimit->totalVolume -= shares;
+//     parentLimit->size -= 1;
+// }
 
 // Execute head order
-void Order::execute()
-{
-    parentLimit->headOrder = nextOrder;
-    if (nextOrder == nullptr)
-    {
-        parentLimit->tailOrder = nullptr;
-    } else
-    {
-        nextOrder->prevOrder = nullptr;
-    }
-    nextOrder = nullptr;
-    prevOrder = nullptr;
+// void Order::execute()
+// {
+//     parentLimit->headOrder = nextOrder;
+//     if (nextOrder == nullptr)
+//     {
+//         parentLimit->tailOrder = nullptr;
+//     } else
+//     {
+//         nextOrder->prevOrder = nullptr;
+//     }
+//     nextOrder = nullptr;
+//     prevOrder = nullptr;
 
-    parentLimit->totalVolume -= shares;
-    parentLimit->size -= 1;
-}
+//     parentLimit->totalVolume -= shares;
+//     parentLimit->size -= 1;
+// }
 
 void Order::modifyOrder(int newShares, int newLimit)
 {
     shares = newShares;
     limit = newLimit;
-    nextOrder = nullptr;
-    prevOrder = nullptr;
-    parentLimit = nullptr;
+    // nextOrder = nullptr;
+    // prevOrder = nullptr;
+    // parentLimit = nullptr;
 }
 
 void Order::setShares(int newShares)
@@ -93,9 +95,10 @@ void Order::setShares(int newShares)
 
 void Order::print() const
 {
-    std::cout << "Order ID: " << idNumber 
-    << ", Order Type: " << (buyOrSell == 1 ? "buy" : "sell") 
-    << ", Order Size: " << shares
-    << ", Order Limit: " << limit 
-    << std::endl;
+    std::cout << "Order ID: " << idNumber
+        << ", Order Type: " << (buyOrSell == 1 ? "buy" : "sell")
+        << ", Order Size: " << shares
+        << ", Order Limit: " << limit
+        << std::endl;
 }
+
